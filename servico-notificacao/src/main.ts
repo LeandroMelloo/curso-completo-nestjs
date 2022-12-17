c45423ef2d4f2ca1b972c92e5a18f13c4a204a58
LeandroMelloo <leandromoreno1909@hotmail.com>
2022-12-16 23:36:02 -0300

    Criando a validação para o projeto

--- servico-notificacao/src/main.ts
+++ servico-notificacao/src/main.ts
@@ -1,8 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe()); // import para validar a entrada dos dados no body da requisição
+
   await app.listen(3033);
 }
 bootstrap();
