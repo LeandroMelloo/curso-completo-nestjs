3c7c5adcbb8efd3b66d3b7dd0e60f10a997195cd
LeandroMelloo <leandromoreno1909@hotmail.com>
2022-12-22 21:58:27 -0300

    principios do solid

---
+++ solid/solid.ts
@@ -0,0 +1,10 @@
+/*  Open Closed Principle: esse princípio diz que podemos adicionar 
+    comportamento em uma classe e em um componente, sem alterar o objeto/classe
+    Dependency Inversion
+    Diferença: Class (Tipo) instância (Objeto)
+    Decorator
+    Composite
+    Proxy
+*/
+
+
